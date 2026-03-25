a64393b46bca732bf35935637c0b55af3e32f2a1
jong-jang <jjh0299@gmail.com>
2026-03-25 21:57:24 +0900

    Initial commit

---
+++ src/main/java/com/jjh/ggumu/GgumuApplication.java
@@ -0,0 +1,13 @@
+package com.jjh.ggumu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgumuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgumuApplication.class, args);
+    }
+
+}
